8103e70a23437ab42d85a7fb6845151006f90a2b
Sahif05 <106846355+Sahif05@users.noreply.github.com>
2025-09-24 10:02:02 -0400

    Update addContact.php

--- api/addContact.php
+++ api/addContact.php
@@ -1,11 +1,10 @@
 <?php
-	$data = getRequestInfo();
 	
-	$first_name = $data['first_name'];
-    $last_name = $data['last_name'];
-    $phone = $data['phone_number'];
-    $email = $data['email'];
-    $id = $data['user_id'];
+	$first_name = $POST['first_name'];
+    $last_name = $POST['last_name'];
+    $phone = $POST['phone_number'];
+    $email = $POST['email'];
+    $id = $POST['user_id'];
 
 	$conn = new mysqli("localhost", "group16", "welovegroup16", "COP4331_lamp_group_16");
 	if ($conn->connect_error) 
@@ -21,10 +20,6 @@
 		$conn->close();
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
 
 	function sendResultInfoAsJson( $obj )
 	{
